753a0689f5b6bb741197f0ad342ec99635b8e87b
Ivan Navarrete <soporte@micronics.mx>
2020-05-28 00:14:35 -0600

    Added rocomendation of games endpoint

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-exports.DATABASE_URL = "mongodb+srv://games:CSFaPOSCeAaMj41R@cluster0-tzjne.mongodb.net/videogamesdb?retryWrites=true&w=majority"//process.env.DATABASE_URL || 'mongodb://localhost/videogamesdb';
+exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/videogamesdb';
 exports.TOKEN = process.env.TOKEN || '2abbf7c3-245b-404f-9473-ade729ed4653';
 exports.PORT = process.env.PORT || '8080';
 exports.SECRET_TOKEN = process.env.SECRET_TOKEN || 'secret';
 
-//mongodb+srv://bookmark:book123@cluster0-uuteo.mongodb.net/bookmarksdb?retryWrites=true&w=majority
+//"mongodb+srv://games:CSFaPOSCeAaMj41R@cluster0-tzjne.mongodb.net/videogamesdb?retryWrites=true&w=majority"
