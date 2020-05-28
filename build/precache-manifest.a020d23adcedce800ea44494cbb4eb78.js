c436b80e6aff51158d6935310a4e410b99299269
Ivan Navarrete <soporte@micronics.mx>
2020-05-28 00:29:02 -0600

    Added rocomendation of games endpoint

---
+++ build/precache-manifest.a020d23adcedce800ea44494cbb4eb78.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae373566785a27802898114b056bf93b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0380f6137922f9f2c3b0",
+    "url": "/static/css/2.ffd39b99.chunk.css"
+  },
+  {
+    "revision": "89fdfbb0ef31881b8e44",
+    "url": "/static/css/main.1dbb0bb4.chunk.css"
+  },
+  {
+    "revision": "0380f6137922f9f2c3b0",
+    "url": "/static/js/2.41ac2c90.chunk.js"
+  },
+  {
+    "revision": "ed6d44160be3da7ea632241f05f7ba30",
+    "url": "/static/js/2.41ac2c90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89fdfbb0ef31881b8e44",
+    "url": "/static/js/main.970f4b97.chunk.js"
+  },
+  {
+    "revision": "4cf42ced02eaeeb742a5",
+    "url": "/static/js/runtime-main.5cd806e3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
